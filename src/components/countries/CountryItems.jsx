9b2f4c4b44ff9dc1e104b411218518528f50b2fc
gitwalker4 <tolbertwalker4@gmail.com>
2022-03-30 14:45:23 -0700

    changed background styles

--- src/components/countries/CountryItems.jsx
+++ src/components/countries/CountryItems.jsx
@@ -2,28 +2,7 @@ import React from 'react'
 
 const CountryItems = ({item}) => {
 
-  let languagesArray = []
-  let bordersArray = []
-
-  if ( item.name.common !== 'Antarctica') {
-    languagesArray.push(Object.values(item.languages))
-  } 
-    
-  if ( item.borders ) {
-    bordersArray.push(Object.values(item.borders))
-  }
-
-  const setLanguages = languagesArray.map((language, index) => {
-    return <li className='list-item' key={index}>{`${language}`}</li>
-  })
-
-  const setBorders = bordersArray.map((border, index)=> {
-    return <li className='list-item' key={index}>{`${border}`}</li>
-  })
-
-  console.log(setBorders)
-
-  return (item.borders) ? (
+  return (
     <div className='card' loading='lazy'>
       <div className='card-inner'>
         <div className='card-front'>
@@ -31,37 +10,24 @@ const CountryItems = ({item}) => {
         </div>
         <div className='card-back'>
           <h1>{item.name.common}</h1>
-          <h3>Capital: {item.capital}</h3>
-          <h3>Population: {item.population}</h3>
-          <ul>
-            <h3 className='list-header'>Official Language(s): </h3>  
-            {setLanguages}
-          </ul>
-          <ul>
-            <h3 className='list-header'>Borders:</h3>
-            {setBorders}
-          </ul>
-          <a href={item.maps.googleMaps} target='_blank' rel='noreferrer' className='map-link'>See on a map</a>
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div className='card'>
-      <div className='card-inner'>
-        <div className='card-front'>
-          <img src={item.flags.svg} alt='' />
-        </div>
-        <div className='card-back'>
-          <h1>{item.name.common}</h1>
-          <h3>Capital: {item.capital}</h3>
-          <h3>Population: {item.population}</h3>
-          <ul>
-            <h3 className='list-header'>Official Language(s): </h3>  
-            {setLanguages}
-          </ul>
-          <div>
-            <h3 className='list-header'>Borders: </h3>
-            <p className='list-item'>None</p>
+          <div className='card-div'>
+            <h2>Capital: </h2>
+            <p>{item.capital}</p>
+          </div>
+          <div className='card-div'>
+            <h2>Population: </h2>
+            <p>{item.population}</p>
+          </div>
+          <div className='card-div'>
+            <h2 className='list-header'>Official Language(s): </h2>
+            {item.languages &&
+              Object.values(item.languages).map((language,index) => {
+              return <p key={index}>{language}</p>
+            })}  
+          </div>
+          <div className='card-div'>
+            <h2>Continent: </h2>
+            <p>{item.continents}</p>
           </div>
           <a href={item.maps.googleMaps} target='_blank' rel='noreferrer' className='map-link'>See on a map</a>
         </div>
